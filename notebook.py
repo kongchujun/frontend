9e9618da61a600bfe4f341e39f41bb0df6dcafbe
chujun <985145157@qq.com>
2025-08-29 17:14:14 +0800

    add

---
+++ notebook.py
@@ -0,0 +1,27 @@
+import marimo
+
+__generated_with = "0.15.1"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""## Hello world""")
+    return
+
+
+@app.cell
+def _(mo):
+    slider = mo.ui.slider(1,10,1)
+    slider
+    return
+
+
+if __name__ == "__main__":
+    app.run()
